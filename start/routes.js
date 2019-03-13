dbe9e7058dfb2c36d4f663b0cdb2576718c7c398
georges tchianga <georgestcm@gmail.com>
2019-03-12 23:15:55 -0400

    welcome page changed to index

--- start/routes.js
+++ start/routes.js
@@ -16,5 +16,5 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+Route.on('/').render('index')
 Route.any('*',"renderController.renderError")
