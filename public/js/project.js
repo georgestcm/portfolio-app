e7c3caa0671d4b3ff0fb93bfd28b8e153a8024a7
georges tchianga <georgestchianga@georgess-MacBook-Pro.local>
2019-06-11 14:45:32 -0400

    project slide up page removal

--- public/js/project.js
+++ public/js/project.js
@@ -1,40 +1,10 @@
 $(function(){
 
 
-  var $livelinks = $('a.more_info');
-  var $projectPageExit = $('a.project_exit')
-  var $projectPage = $('div.project_displayer');
-
-
-
-var position;
-
-  $livelinks.on('click',(e)=>{
-    position = $(this).scrollTop();
-    console.log(position)
-    $('div.nav').css('zIndex',0)
-    $('div.landing_parent').hide();
-    $('div.main_page').fadeOut('fast')
-    e.preventDefault();
-    $projectPage.animate({
-      height: '100vh'
-    },()=>{
-      $projectPage.css('display','contents')
-    })
-  })
-
-  $projectPageExit.on('click',(e)=>{
+  var $liveLinks = $('div.pro_temp_links a.more_info');
+  console.log($liveLinks)
+$liveLinks.on('click',(e)=>{
   e.preventDefault();
-console.log(position)
-  $projectPage.animate({
-    height: '0vh'
-  },100,function(){
-    $(this).css({
-      'display':''
-    })
-    $('div.main_page').fadeIn();
-  })
-
-  })
-
+  console.log(e)
+})
 })
